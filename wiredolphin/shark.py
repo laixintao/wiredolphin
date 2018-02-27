c85c17c6e904b3e14768a7ee452d837ef11abbca
laixintao <glenn.lai@bosondata.com.cn>
2018-02-27 23:29:51 +0800

    add Time column

--- wiredolphin/shark.py
+++ wiredolphin/shark.py
@@ -17,14 +17,28 @@ logger = logging.getLogger("shark")
 
 class DolphinPacket(dict):
     """Wrap pyshark's packet, to display in datatable"""
+    time_start = None
+
+    @classmethod
+    def get_time_delta(cls, sniff_timestamp):
+        # TODO possiable to use tshark's time_relative?
+        # https://www.wireshark.org/lists/wireshark-users/201110/msg00143.html
+        sniff_timestamp = float(sniff_timestamp)
+        if cls.time_start is None:
+            cls.time_start = sniff_timestamp
+            return 0
+        else:
+            return sniff_timestamp - cls.time_start
 
     def __init__(self, packet):
         self._packet = packet
         super().__init__()
+
         self.update({
             "highest_layer": packet.highest_layer,
             'number': packet.number,
             'length': packet.length,
+            'time_delta': "%.6f" % self.get_time_delta(packet.sniff_timestamp),
         })
 
         if 'tcp' in packet:
