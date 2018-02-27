cc75091db634c8f67829a09564396fffa20f297b
laixintao <glenn.lai@bosondata.com.cn>
2018-02-28 00:31:13 +0800

    list table use only_summary=True

--- wiredolphin/shark.py
+++ wiredolphin/shark.py
@@ -7,6 +7,7 @@ Load pcapng using pyshark
 from pyshark.capture.file_capture import FileCapture
 
 
+from ipaddress import ip_address
 import logging
 logging.basicConfig(
     level=logging.DEBUG,
@@ -15,42 +16,16 @@ logging.basicConfig(
 logger = logging.getLogger("shark")
 
 
-class DolphinPacket(dict):
-    """Wrap pyshark's packet, to display in datatable"""
-    time_start = None
-
-    @classmethod
-    def get_time_delta(cls, sniff_timestamp):
-        # TODO possiable to use tshark's time_relative?
-        # https://www.wireshark.org/lists/wireshark-users/201110/msg00143.html
-        sniff_timestamp = float(sniff_timestamp)
-        if cls.time_start is None:
-            cls.time_start = sniff_timestamp
-            return 0
-        else:
-            return sniff_timestamp - cls.time_start
-
-    def __init__(self, packet):
-        self._packet = packet
-        super().__init__()
-
-        self.update({
-            "highest_layer": packet.highest_layer,
-            'number': packet.number,
-            'length': packet.length,
-            'time_delta': "%.6f" % self.get_time_delta(packet.sniff_timestamp),
-            'src': packet.ip.src,
-            'dst': packet.ip.dst,
-        })
-
-        if 'tcp' in packet:
-            self.update(tcp_port=packet['tcp'].port)
-
-    @property
-    def transport_layer_and_heighest_layer(self):
-        return '%s/%s' % (self._packe.transport_protocol, self._packet.highest_layer)
-
-
 def packet_lists():
-    packets = FileCapture("test.pcapng")
-    return [DolphinPacket(packet) for packet in packets]
+    packets = FileCapture("test.pcapng", only_summaries=True)
+    # https://docs.python.org/3/library/ipaddress.html
+    for packet in packets:
+        yield {
+            'Destination': ip_address(packet._fields['Destination']),
+            'Source': ip_address(packet._fields['Source']),
+            'Length': int(packet._fields['Length']),
+            'No.': int(packet._fields['No.']),
+            'Protocol': packet._fields['Protocol'],
+            'Time': float(packet._fields['Time']),
+            'Info': packet._fields['Info'],
+        }
